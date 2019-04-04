8f5fffe140afc8b8c7ff004435a7ec77fcf50478
Longhorn <47125908+tribetrb@users.noreply.github.com>
2019-04-04 08:21:09 -0500

    Update ADCSwitch.ino

--- ADCSwitch.ino
+++ ADCSwitch.ino
@@ -1,4 +1,7 @@
-
+//Analog Input Voltage Pin: A0 Digital Output Pin: Pin 12 I am also switching LED on Pin 13 ON/OFF
+//Calculations: With 10K, 4.7K Voltage divider, when you take voltage off of 4.7K, following is the rough voltage that gets fed to Arduino Input.
+//Voltage -- Arduino Input from 4.7K 12.0V -- 3.836V 12.5V -- 3.996V Approx 4Volts
+//3.83 and 4.0V are used for switching between ON/OFF states. Serial Monitor at 9600 baud shows rough voltage.
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int outPin = 12;
