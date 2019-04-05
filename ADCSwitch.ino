33cdb2d45c2cd9e4fb6f4b0a3981bfbb7b53bf56
Longhorn <47125908+tribetrb@users.noreply.github.com>
2019-04-05 07:15:54 -0500

    Update ADCSwitch.ino

--- ADCSwitch.ino
+++ ADCSwitch.ino
@@ -2,6 +2,11 @@
 //Calculations: With 10K, 4.7K Voltage divider, when you take voltage off of 4.7K, following is the rough voltage that gets fed to Arduino Input.
 //Voltage -- Arduino Input from 4.7K 12.0V -- 3.836V 12.5V -- 3.996V Approx 4Volts
 //3.83 and 4.0V are used for switching between ON/OFF states. Serial Monitor at 9600 baud shows rough voltage.
+#include<avr/wdt.h> /* Header for watchdog timers in AVR */
+
+//#define DEBUG true
+#define DEBUG false
+
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 13;      // select the pin for the LED
 int outPin = 12;
@@ -13,14 +18,21 @@ int switchState = 0;
 
 void setup() {
 
-  Serial.begin(9600);
-  Serial.println("Program Start");
+  if(DEBUG) {
+    Serial.begin(9600);
+    Serial.println("Program Start");    
+  }
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
   pinMode(outPin, OUTPUT);  
   digitalWrite(outPin, LOW); 
   digitalWrite(ledPin, LOW);   
-  
+
+  //Watchdog Code
+  wdt_disable();  /* Disable the watchdog and wait for more than 2 seconds */
+  delay(3000);  /* Done so that the Arduino doesn't keep resetting infinitely in case of wrong configuration */
+  wdt_enable(WDTO_8S);  /* Enable the watchdog with a timeout of 8 seconds */
+ 
 }
 
 void loop() {
@@ -39,9 +51,14 @@ void loop() {
     digitalWrite(outPin, LOW); 
     digitalWrite(ledPin, LOW); 
   }
-  Serial.print("switchState ");  
-  Serial.println(switchState);    
-  Serial.print("ADC Voltage ");    
-  Serial.println(voltage);
-  delay(500);
+  if(DEBUG) {
+    Serial.print("switchState ");  
+    Serial.println(switchState);    
+    Serial.print("ADC Voltage ");    
+    Serial.println(voltage);
+    delay(500);
+  }else {
+    delay(100);    
+  }
+  wdt_reset();  /* Reset the watchdog */
 }
